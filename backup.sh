c6d8557d7fc77b880cb8910aa2404bd136f2baea
Brent <puddletowndesign@gmail.com>
2017-04-02 09:36:27 -0500

    updated brew packages

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 cd ~/Documents/Dev/Brew
 brew list > packages.list
-brew cask list > casks.list
+brew cask list > casks-all.list
 git add .
 git commit -m "updated brew packages"
 git push origin mac
